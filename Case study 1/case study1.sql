1a400922a17350a8b298240fe390886b778865dc
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-07-13 20:50:26 +0700

    case s

--- Case study 1/case study1.sql
+++ Case study 1/case study1.sql
@@ -174,13 +174,68 @@ join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
 where ten_loai_khach = "Diamond"
 group by ma_khach_hang
 order by so_lan_dat_phong;
+
 -- cau 5 
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong,
+dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, dich_vu.chi_phi_thue + ifnull((dich_vu_di_kem.gia*hop_dong_chi_tiet.so_luong),0) as tong_tien
+from khach_hang
+left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+left join hop_dong on khach_hang.ma_khach_hang =  hop_dong.ma_khach_hang  
+left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem;
 
+-- cau 6
+select  dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+left join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+left join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where dich_vu.ma_dich_vu not in (
+select hop_dong.ma_dich_vu
+from hop_dong 
+where year(hop_dong.ngay_lam_hop_dong) = 2021 and quarter(hop_dong.ngay_lam_hop_dong) = 1 )
+group by dich_vu.ma_dich_vu;
 
+-- cau 7
 
+select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.so_nguoi_toi_da,dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+left join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+left join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where dich_vu.ma_dich_vu not in(
+select hop_dong.ma_dich_vu
+from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) = 2021)
+and year(hop_dong.ngay_lam_hop_dong) = 2020
+group by hop_dong.ma_dich_vu;
 
-  
+-- cau 8
+
+select distinct khach_hang.ho_ten 
+from khach_hang; 
+
+select  khach_hang.ho_ten 
+from khach_hang
+group by khach_hang.ho_ten; 
+
+select  khach_hang.ho_ten 
+from khach_hang
+union
+select  khach_hang.ho_ten 
+from khach_hang;
     
-   
+-- cau 9
+
+select month(hop_dong.ngay_lam_hop_dong) as thang, count(hop_dong.ma_khach_hang) as so_khach_dat_phong
+from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) = 2021
+group by thang
+order by thang; 
 	
+-- cau 10
+
+select  hop_dong.ma_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, ifnull(sum(hop_dong_chi_tiet.so_luong),0) as so_luong_dich_vu_di_kem
+from hop_dong
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+group by hop_dong.ma_hop_dong;
      
